4d243d2f8296f2cb81ea056ea2d3bab733942125
imolo <amolokoedova@gmail.com>
2023-04-27 17:18:26 +0300

    task 03 code added

--- task03/Program.cs
+++ task03/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> среда
+// 5 -> пятница
+
+Console.WriteLine("Введите число от 1 до 7:");
+int i = Convert.ToInt32(Console.ReadLine());
+
+if (i == 1)
+{
+    Console.WriteLine ("понедельник");
+}
+else if (i == 2)
+{
+    Console.WriteLine ("вторник");
+}
+else if (i == 3)
+{
+    Console.WriteLine ("среда");
+}
+else if (i == 4)
+{
+    Console.WriteLine ("четверг");
+}
+else if (i == 5)
+{
+    Console.WriteLine ("пятница");
+}
+else if (i == 6)
+{
+    Console.WriteLine ("суббота");
+}
+else if (i == 7)
+{
+    Console.WriteLine ("воскресенье");
+}
+else 
+{
+    Console.WriteLine ("ошибка");
+}
