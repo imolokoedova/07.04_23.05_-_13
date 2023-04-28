b6120cdab18fb3f79ce664936d9200e1f5c3a8f9
imolo <amolokoedova@gmail.com>
2023-04-28 17:17:10 +0300

    task06 code added

---
+++ task06/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и возвращает яыляется ли число четным.
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+Console.WriteLine("Введите число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+    
+else
+{
+    Console.WriteLine("нет");
+}
+    
