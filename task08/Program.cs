c4009eed9f3430d27d7f2ec16f50c3e60090e2d6
imolo <amolokoedova@gmail.com>
2023-04-28 17:40:57 +0300

    task08 code added

---
+++ task08/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число N, а на выходе показывает все четныеы числа от 1 до N.
+// 5 -> 2,4
+// 8 -> 2,4,6,8
+
+Console.WriteLine("Введите любое натуральное число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= numberA)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write(count + " ");
+    }
+
+    count++;
+}
+
