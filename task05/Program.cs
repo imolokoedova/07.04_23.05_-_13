f343adc7342caffded2c971cd42f1e8f5cfc88f7
imolo <amolokoedova@gmail.com>
2023-04-28 14:05:59 +0300

    task05 code added

--- task05/Program.cs
+++ task05/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает одно число (N), а навыходе показывает все целые числа от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> "-2, -1, 0, 1, 2"
+
+Console.WriteLine("Введите числа:");
+int i = Convert.ToInt32(Console.ReadLine());
+int count = -i;
+while (count<=i)
+{
+    Console.Write(count + " ");
+    count ++;
+}
