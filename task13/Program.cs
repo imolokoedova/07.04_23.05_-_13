e39952c2edd2e25b0c3e70be0bc98271e52da339
imolo <amolokoedova@gmail.com>
2023-05-05 01:33:02 +0300

    task13 code added

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+//метод
+int GetThirdDigit(int num)
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    return num % 10;
+}
+
+
+
+
+Console.WriteLine("Введите любое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет.");
+}
+else
+{
+    int result = GetThirdDigit(number);
+    Console.WriteLine ($"Третье число:{result}");
+}
+
+
+
+
+
+
+
