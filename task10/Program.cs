277e6c042f1b7b7b346dd1f2a3ea334f5ada0b9f
imolo <amolokoedova@gmail.com>
2023-05-04 16:03:43 +0300

    task10 code added

---
+++ task10/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int WriteSecondDigit(int num)
+{
+    return (num % 100);
+}
+
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && <= 999) ;
+{
+    int result WriteSecondDigit(int num);
+    Console.WriteLine(result);
+}
+
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число.");
+}
