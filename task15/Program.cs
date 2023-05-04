41d43eeed3c356f667acd5d7d80ec1ed9c6d2386
imolo <amolokoedova@gmail.com>
2023-05-05 02:12:04 +0300

    task15 code added

--- task15/Program.cs
+++ task15/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели и проверяет является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+bool IsDayOff(int num)
+{
+    return (num == 6 || num == 7 ? true : false);
+}
+
+Console.WriteLine("Введите цифру, обозначающую день недели:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1 || number > 7)
+    Console.WriteLine("В неделе вообще-то семь дней.");
+else
+    Console.WriteLine(IsDayOff(number) ? "да" : "нет");
+
+
+
+
+
+
