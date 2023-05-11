9475f1fca516b35becee3701bfa7de5df0657676
imolo <amolokoedova@gmail.com>
2023-05-11 17:50:13 +0300

    task19 code added

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+
+
+
+bool IsPalindrome(int num)
+{
+    int num1 = (num % 100000) / 10000;
+    int num2 = (num % 10000) / 1000;
+    int num4 = (num % 100) / 10;
+    int num5 = num % 10;
+    return num1 == num5 && num2 == num4;
+}
+
+
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0)
+{
+    number = number * -1;
+}
+if (number < 10000 || number > 99999)
+{
+    Console.WriteLine("Вы ввели не пятизначное число.");
+}
+else
+{
+    bool result = IsPalindrome(number);
+    Console.WriteLine(result ? "да" : "нет");
+}
+
+
