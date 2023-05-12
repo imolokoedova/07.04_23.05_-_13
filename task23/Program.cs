ccc9e0a518ea46c6b674abfd2c77ea448795499b
imolo <amolokoedova@gmail.com>
2023-05-12 17:13:21 +0300

    task23 code added

--- task23/Program.cs
+++ task23/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число N и выдает таблицу кубов всех чисел от 1 до N.
+// 3 -> 1, 8, 27;
+// 5 -> 1, 8, 27, 64, 125;
+
+// метод TableAllCubes
+
+void TableAllCubes(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count, 3} {count * count * count, 5}");
+        count++;
+    }
+}
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+TableAllCubes(number);
+
+
