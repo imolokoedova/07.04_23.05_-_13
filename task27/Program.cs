6ab3ff3e794d211bf281e01ac7f926b8ee8b89b4
imolo <amolokoedova@gmail.com>
2023-05-22 00:03:59 +0300

    added code for task27 and changed net7.0 to net6.0

--- task27/Program.cs
+++ task27/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая 
+// 1. принимает на вход число и 
+// 2. выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0)
+    number *= -1;
+
+int splitAndSum = SplitAndSum(number);
+Console.WriteLine(splitAndSum);
+
+// метод
+
+int SplitAndSum(int num)
+{
+    int sum = 0;
+    while (num != 0)
+    {
+        sum += num % 10;
+        num /= 10;
+
+    }
+    return sum;
+}
+
+
+
+
+
+
