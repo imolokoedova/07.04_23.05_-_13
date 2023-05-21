030cfc5ad3b0bdd6cf5c2d3321ff37422999e669
imolo <amolokoedova@gmail.com>
2023-05-22 00:33:02 +0300

    added code for task29 and changed net7.0 to net6.0

--- task29/Program.cs
+++ task29/Program.cs
@@ -4,4 +4,30 @@
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
 
-Console.WriteLine("Hello, World!");
+int[] array = CreateArray (8);
+PrintArray (array);
+Console.Write ("-> [ ");
+PrintArray (array);
+Console.Write ("]");
+
+
+int[] CreateArray (int size)
+{
+    int[] arr = new int [size];
+    Random random = new Random ();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = random.Next (0, 100);
+    }
+    return arr;
+}
+
+// метод 2
+
+void PrintArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write ($"{arr[i]} ");
+    }
+}
