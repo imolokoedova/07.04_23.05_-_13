9250f8d40e9c6fc460ba471895e6f1d122747b56
imolo <amolokoedova@gmail.com>
2023-05-21 23:30:54 +0300

    added code task25 & changed net7.0 to net.6.0

--- task25/Program.cs
+++ task25/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который 
+// 1. принимает на вход два числа (A и B) и 
+// 2. возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+
+Console.WriteLine("Введите число А: ");
+double numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите целое положительное число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberB < 0)
+{
+    Console.WriteLine ("Вы ввели отрицательное число B. Число B должно быть положительным.");
+    return;
+}
+
+
+double raiseToPower = RaiseToPower (numberA, numberB);
+Console.WriteLine (raiseToPower);
+
+
+// метод
+
+double RaiseToPower(double numA, int numB)
+{
+    double result = 1;
+    for (int i = 0; i < numB; i++)
+    {
+        checked
+        {
+            result *= numA;
+        }
+
+    }
+    return result;
+
+}
