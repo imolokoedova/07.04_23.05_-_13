90cc8ea04fbcfd79ebc5e47c8cdfd7cecc0386b3
imolo <amolokoedova@gmail.com>
2023-05-22 01:06:32 +0300

    added code for task34 and changed net7.0 to 6.0

--- task34/Program.cs
+++ task34/Program.cs
@@ -3,5 +3,49 @@
 
 // [345, 897, 568, 234] -> 2
 
+int[] array = CreateArrayRndInt (5, 100, 1000);
+PrintArray (array);
+int findAmmountOfEvens = FindAmmountOfEvens (array);
+Console.Write ($" -> {findAmmountOfEvens}");
 
-Console.WriteLine("Hello, World!");
+
+
+// метод 1
+
+int[] CreateArrayRndInt (int size, int min, int max)
+{
+    int[] arr = new int [size];
+    Random rnd = new Random();
+    
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next (min, max+1);
+    }
+    return arr;
+}
+
+// метод 2
+
+void PrintArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+        Console.Write ($"{arr[i]}, ");
+        else
+        Console.WriteLine ($"{arr[i]}");
+    }
+}
+
+// метод 3
+
+int FindAmmountOfEvens (int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i]%2 == 0)
+        count ++;
+    }
+    return count;
+}
