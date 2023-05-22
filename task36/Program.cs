de5cdf978f4988683612d67cdfd63f43c33b4f04
imolo <amolokoedova@gmail.com>
2023-05-22 16:14:48 +0300

    added code for task36 and changed net7.0 to 6.0

--- task36/Program.cs
+++ task36/Program.cs
@@ -4,4 +4,54 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
-Console.WriteLine("Hello, World!");
+
+int[] array = CreateArrayRndInt(5, 0, 100);
+Console.Write("[");
+PrintArray(array);
+Console.Write("] -> ");
+int findSumOfOdds = FindSumOfOdds(array);
+Console.WriteLine(findSumOfOdds);
+
+
+// метод 1
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+// метод 2
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}, ");
+        else
+            Console.Write($"{arr[i]}");
+    }
+}
+
+// метод 3
+
+int FindSumOfOdds(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += arr[i];
+        }
+
+    }
+    return sum;
+}
