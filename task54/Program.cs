029384ad0074db37d0cdddc1f37b5746737d500f
imolo <amolokoedova@gmail.com>
2023-05-26 04:05:03 +0300

    added code for task54 and changed net7.0 to 6.0

--- task54/Program.cs
+++ task54/Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] matrix = CreateMatrixRndInt(4, 4, -10, 10);
+PrintMatrix(matrix);
+Console.WriteLine();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    SortElementsToMin(matrix, i);
+}
+PrintMatrix(matrix);
+
+
+// метод 1
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+
+// метод 2
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+// метод 3
+
+void SortElementsToMin(int[,] matrix, int i)
+{
+    for (int k = 0; k < matrix.GetLength(1); k++)
+    {
+        for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+        {
+            if (matrix[i, j] < matrix[i, j + 1])
+            {
+                int tmp = matrix[i, j + 1];
+                matrix[i, j + 1] = matrix[i, j];
+                matrix[i, j] = tmp;
+            }
+        }
+    }
+
+}
+
