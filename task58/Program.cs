ae60d6c67e715916586e701daf9d54eee1bfd920
imolo <amolokoedova@gmail.com>
2023-05-28 01:27:41 +0300

    fixed code for task58 and changed net7.0 to 6.0

--- task58/Program.cs
+++ task58/Program.cs
@@ -4,20 +4,22 @@
 // 3 2 | 3 3
 // Результирующая матрица будет:
 // 18 20
-// 15 18
+// 15 18 
 
 
-int[,] matrix1 = CreateMatrixRndInt(4, 3, -10, 10);
-PrintMatrix(matrix1);
-Console.WriteLine();
-int[,] matrix2 = CreateMatrixRndInt(2, 3, -10, 10);
-PrintMatrix(matrix2);
+int[,] matrix1 = CreateMatrixRndInt(3, 3, 0, 9);
+int[,] matrix2 = CreateMatrixRndInt(3, 3, 0, 9);
+PrintMatrix2(matrix1, matrix2);
 Console.WriteLine();
 if (matrix1.GetLength(1) != matrix2.GetLength(0))
-Console.WriteLine ("Матрицы не перемножаемы.");
-return;
-int[,] rezultMatrix = MultiplyMatrix(matrix1, matrix2);
-PrintMatrix(rezultMatrix);
+{
+    Console.WriteLine("Матрицы не перемножаемы.");
+    return;
+}
+int[,] resultMatrix = MultiplyMatrix(matrix1, matrix2);
+Console.WriteLine("Результирующая матрица будет:");
+Console.WriteLine();
+PrintMatrix(resultMatrix);
 
 
 // метод 1
@@ -51,22 +53,42 @@ void PrintMatrix(int[,] matrix)
     }
 }
 
+
+void PrintMatrix2(int[,] matrix1, int[,] matrix2)
+{
+    for (int i = 0; i < matrix1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix1.GetLength(1); j++)
+        {
+            Console.Write($"{matrix1[i, j],2}");
+        }
+
+        Console.Write("  | ");
+
+        for (int j = 0; j < matrix2.GetLength(1); j++)
+        {
+            Console.Write($"{matrix2[i, j],2}");
+        }
+        Console.WriteLine();
+    }
+}
+
 // метод 3
 
 int[,] MultiplyMatrix(int[,] matrix1, int[,] matrix2)
 {
-    int[,] rezultMatrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
-    for (int i = 0; i < rezultMatrix.GetLength(0); i++)
+    int[,] resultMatrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
     {
-        for (int j = 0; j < rezultMatrix.GetLength(1); j++)
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
         {
-            int sum = 0;
+            int mult = 0;
             for (int k = 0; k < matrix1.GetLength(1); k++)
             {
-                sum += matrix1[i, k] * matrix2[k, j];
+                mult += matrix1[i, k] * matrix2[k, j];
             }
-            rezultMatrix[i, j] = sum;
+            resultMatrix[i, j] = mult;
         }
     }
-    return rezultMatrix;
+    return resultMatrix;
 }
